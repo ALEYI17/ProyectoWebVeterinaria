c664014ba55e07cb978c628d68c37a08f082ec1b
Andres-Salamanca <andrealebarbaritos@gmail.com>
2023-08-26 11:55:17 -0500

    se hizo el login cleinte cuando no se encuiantra en la base de datos falta hacer que se redireccione a la pagina de cliente con sus mascotas

--- src/main/java/com/vetcare/proyecto/controlador/logInController.java
+++ src/main/java/com/vetcare/proyecto/controlador/logInController.java
@@ -2,6 +2,7 @@ package com.vetcare.proyecto.controlador;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.vetcare.proyecto.entities.Cliente;
+import com.vetcare.proyecto.service.ClienteServicio;
 
 @Controller
 public class logInController {
+
+    @Autowired
+    ClienteServicio clienteServicio;
 
     Logger log = LoggerFactory.getLogger(getClass());
     @GetMapping("/login")
@@ -23,7 +28,8 @@ public class logInController {
 
     
     @GetMapping("/login/cliente")
-    public String mostrarPaginaLoginCliente(){
+    public String mostrarPaginaLoginCliente(Model model){
+
         return "LoginCliente";
     }
 
@@ -32,6 +38,11 @@ public class logInController {
         // Now you can access the cedula attribute of the cliente object
         String cedula = cliente.getCedula();
         log.info(cedula);
+        Cliente clienteLogIn = clienteServicio.GetById(cedula);
+        if(clienteLogIn == null){
+            model.addAttribute("errorMessage", "Invalid login credentials");
+            return "LoginCliente"; // Return to the login page with an error message
+        }
         // Perform necessary business logic using cedula
         
         return "redirect:/cliente"; // Redirect to a success page after processing
